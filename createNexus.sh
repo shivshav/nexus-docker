bbf3e8371b926b348a082a55f0acd8a8e473be52
shiv <sprasad0603@gmail.com>
2015-10-16 23:06:17 -0700

    Added line to create the plugin-repository subdir so that the nexus-start script doesnt error out on first run

--- createNexus.sh
+++ createNexus.sh
@@ -3,12 +3,14 @@ set -e
 NEXUS_NAME=${NEXUS_NAME:-nexus}
 NEXUS_VOLUME=${NEXUS_VOLUME:-nexus-volume}
 NEXUS_IMAGE_NAME=${NEXUS_IMAGE_NAME:-openfrontier/nexus}
+SONATYPE_WORK=/sonatype-work
 
 #Create nexus volume.
 if [ -z "$(docker ps -a | grep ${NEXUS_VOLUME})" ]; then
     docker run \
     --name ${NEXUS_VOLUME} \
     ${NEXUS_IMAGE_NAME} \
+    mkdir ${SONATYPE_WORK}/plugin-repository
     echo "Create nexus volume."
 fi
 
