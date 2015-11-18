265c1a3551cc3cdbed6d8e2745e5a0a2de007b02
shiv <sprasad0603@gmail.com>
2015-11-18 13:50:04 -0800

    Added nexus templates and default configuration files to be copied by the new setupNexus script that is run after creation

---
+++ setupNexus.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+BASEDIR=$(readlink -f $(dirname $0))
+set -e
+
+LDAP_SERVER=${1:-openldap}
+SLAPD_DOMAIN=${2:-demo.com}
+LDAP_ACCOUNTBASE=${3:-ou=accounts,dc=demo,dc=com}
+NEXUS_NAME=${4:-nexus}
+
+NEXUS_CONFIF_DIR=/sonatype-work/conf
+LDAP_CONFIG_NAME=ldap.xml
+SECURITY_CONFIG_NAME=security.xml
+SECURITY_CONFIG_EXTRAA_NAME=security-configuration.xml
+#Convert FQDN to LDAP base DN
+SLAPD_TMP_DN=".${SLAPD_DOMAIN}"
+while [ -n "${SLAPD_TMP_DN}" ]; do
+SLAPD_DN=",dc=${SLAPD_TMP_DN##*.}${SLAPD_DN}"
+SLAPD_TMP_DN="${SLAPD_TMP_DN%.*}"
+done
+SLAPD_DN="${SLAPD_DN#,}"
+
+
+LDAP_ACCOUNTBASE="$( cut -d ',' -f 1 <<< "$LDAP_ACCOUNTBASE" )"
+
+# Create configs from template
+echo "Creating template files..."
+sed -e "s/{SLAPD_DN}/${SLAPD_DN}/g" ${BASEDIR}/${LDAP_CONFIG_NAME}.template > ${BASEDIR}/${LDAP_CONFIG_NAME}
+sed -i "s/{LDAP_HOST}/${LDAP_SERVER}/g" ${BASEDIR}/${LDAP_CONFIG_NAME}
+sed -i "s/{LDAP_ACCOUNTBASE}/${LDAP_ACCOUNTBASE}/g" ${BASEDIR}/${LDAP_CONFIG_NAME}
+
+# Copy over relevant configs
+echo "Copying over Nexus configurations..."
+docker cp ${BASEDIR}/${LDAP_CONFIG_NAME} ${NEXUS_NAME}:${NEXUS_CONFIF_DIR}
+docker cp ${BASEDIR}/${SECURITY_CONFIG_NAME} ${NEXUS_NAME}:${NEXUS_CONFIF_DIR}
+docker cp ${BASEDIR}/${SECURITY_CONFIG_EXTRAA_NAME} ${NEXUS_NAME}:${NEXUS_CONFIF_DIR}
+
+echo "Restarting ${NEXUS_NAME} container..."
+docker restart ${NEXUS_NAME}
