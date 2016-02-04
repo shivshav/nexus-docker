29cfc092357e9d8a19fe5722a527ced7a53693d0
shiv <sprasad0603@gmail.com>
2016-02-04 00:40:11 -0800

    Removing the destroy errors

--- destroyNexus.sh
+++ destroyNexus.sh
@@ -3,8 +3,10 @@
 NEXUS_NAME=${NEXUS_NAME:-nexus}
 NEXUS_VOLUME=${NEXUS_VOLUME:-nexus-volume}
 
-if [ -n "$(docker ps -a | grep ${NEXUS_NAME})" ]; then
-docker stop ${NEXUS_NAME}
-docker rm -v ${NEXUS_NAME}
-docker rm -v ${NEXUS_VOLUME}
-fi
+echo "Removing ${NEXUS_NAME}..."
+docker stop ${NEXUS_NAME} &> /dev/null
+docker rm -v ${NEXUS_NAME} &> /dev/null
+
+echo "Removing ${NEXUS_VOLUME}..."
+docker rm -v ${NEXUS_VOLUME} &> /dev/null
+
